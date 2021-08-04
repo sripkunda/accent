b5577382326ac40faadab5cd2432911ace4f4b11
SriKoala <33268626+sripkunda@users.noreply.github.com>
2021-08-04 14:29:04 -0700

    Worked on docs

--- docs/src/components/HomepageFeatures.js
+++ docs/src/components/HomepageFeatures.js
@@ -5,7 +5,6 @@ import styles from './HomepageFeatures.module.css';
 const FeatureList = [
   {
     title: 'Easy to Use',
-    Svg: require('../../static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
         Accent is built for easy and fast development, with a simple API built for applications of all sizes.
@@ -14,7 +13,6 @@ const FeatureList = [
   },
   {
     title: 'Fully Modular',
-    Svg: require('../../static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
         Accent is made up of different standalone modules, or "libraries" that work together. You use only what you need for your project.
@@ -23,7 +21,6 @@ const FeatureList = [
   },
   {
     title: 'Lightweight',
-    Svg: require('../../static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
         Accent is fast and allows you to write fully optimized and secure web applications in minutes.
@@ -36,7 +33,6 @@ function Feature({Svg, title, description}) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
-        <Svg className={styles.featureSvg} alt={title} />
       </div>
       <div className="text--center padding-horiz--md">
         <h3>{title}</h3>
